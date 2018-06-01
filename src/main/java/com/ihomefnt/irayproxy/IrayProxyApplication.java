c2b1dea651854d72babe828ea470aaf391737ba8
zhangrong <brian.rongzhang@gmail.com>
2018-06-01 15:50:37 +0800

    init

--- src/main/java/com/ihomefnt/irayproxy/IrayProxyApplication.java
+++ src/main/java/com/ihomefnt/irayproxy/IrayProxyApplication.java
@@ -1,11 +1,13 @@
 package com.ihomefnt.irayproxy;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableAutoConfiguration
 public class IrayProxyApplication {
 
 	public static void main(String[] args) {
