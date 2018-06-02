5c655cbc5355ab7b45095bd8dc59d1cc831d457f
zhangrong <brian.rongzhang@gmail.com>
2018-06-02 14:03:14 +0800

    init

--- src/main/java/com/ihomefnt/irayproxy/IrayProxyApplication.java
+++ src/main/java/com/ihomefnt/irayproxy/IrayProxyApplication.java
@@ -4,14 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.stream.config.BinderFactoryConfiguration;
-import org.springframework.cloud.stream.config.BindingServiceConfiguration;
-import org.springframework.cloud.stream.config.ChannelBindingAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableConfigServer
-@ComponentScan(basePackages = {"com.ihomefnt.irayproxy"})
 public class IrayProxyApplication {
 
 	public static void main(String[] args) {
