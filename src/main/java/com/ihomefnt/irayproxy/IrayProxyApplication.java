b66a37796a1f54dfa448c72de58c3a66cf1c48b3
zhangrong <brian.rongzhang@gmail.com>
2018-06-02 09:22:42 +0800

    init

--- src/main/java/com/ihomefnt/irayproxy/IrayProxyApplication.java
+++ src/main/java/com/ihomefnt/irayproxy/IrayProxyApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.stream.config.BinderFactoryConfiguration;
+import org.springframework.cloud.stream.config.BindingServiceConfiguration;
+import org.springframework.cloud.stream.config.ChannelBindingAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
--- src/main/java/com/ihomefnt/irayproxy/config/ServerListProperties.java
+++ src/main/java/com/ihomefnt/irayproxy/config/ServerListProperties.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
 
 import com.google.common.collect.Lists;
@@ -14,6 +15,7 @@ import lombok.Data;
 
 @Configuration
 @ConfigurationProperties(prefix="iray")
+@RefreshScope
 @Data
 public class ServerListProperties {
 	List<String> server = Lists.newArrayList();
