191563e8f9fdb7e7d80cee6761793eb1759412c9
zhangrong <brian.rongzhang@gmail.com>
2018-06-04 15:33:19 +0800

    init

--- src/main/java/com/ihomefnt/irayproxy/controller/IrayProxyController.java
+++ src/main/java/com/ihomefnt/irayproxy/controller/IrayProxyController.java
@@ -4,22 +4,17 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.google.common.collect.Lists;
-import com.ihomefnt.irayproxy.config.ServerListProperties;
 import com.ihomefnt.irayproxy.loadbalance.LoadBalanceConfig;
 import com.ihomefnt.irayproxy.loadbalance.UnsupportLoadBalanceException;
 import com.ihomefnt.irayproxy.utils.IhomeConfigUtils;
 
 @RestController
 public class IrayProxyController {
-
-	@Autowired
-	ServerListProperties properties;
 
 	@RequestMapping(method = RequestMethod.GET, value = "irayCloud")
 	public String hello(HttpServletRequest request) throws UnsupportLoadBalanceException {
