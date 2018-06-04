ea4fa5fca82d0c7522823c924cf665dc44fe2e3d
zhangrong <brian.rongzhang@gmail.com>
2018-06-04 11:11:37 +0800

    init

--- src/main/java/com/ihomefnt/irayproxy/controller/IrayProxyController.java
+++ src/main/java/com/ihomefnt/irayproxy/controller/IrayProxyController.java
@@ -1,22 +1,27 @@
 package com.ihomefnt.irayproxy.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.google.common.base.Joiner;
+import com.google.common.collect.Lists;
 import com.ihomefnt.irayproxy.config.ServerListProperties;
 import com.ihomefnt.irayproxy.utils.IhomeConfigUtils;
 
 @RestController
 public class IrayProxyController {
-	
+
 	@Autowired
-	ServerListProperties  properties;
-	
-	@RequestMapping(method=RequestMethod.GET,value="hello")
-	public String hello(){
-		return Joiner.on(",").join(IhomeConfigUtils.getServerList());
+	ServerListProperties properties;
+
+	@RequestMapping(method = RequestMethod.GET, value = "hello")
+	public String hello() {
+		List<String> serverList = Lists.newArrayList();
+		IhomeConfigUtils.parseServeList(serverList);
+		return Joiner.on(",").join(serverList);
 	}
 }
