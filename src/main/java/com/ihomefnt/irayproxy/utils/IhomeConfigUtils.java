203d9715f07c8ba1ba36269bf9074e25698d2e8b
zhangrong <brian.rongzhang@gmail.com>
2018-06-05 09:37:49 +0800

    init

--- src/main/java/com/ihomefnt/irayproxy/utils/IhomeConfigUtils.java
+++ src/main/java/com/ihomefnt/irayproxy/utils/IhomeConfigUtils.java
@@ -8,7 +8,6 @@ import java.util.concurrent.TimeUnit;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 
-import com.google.common.base.Joiner;
 import com.google.common.base.Splitter;
 
 import lombok.extern.slf4j.Slf4j;
@@ -36,41 +35,43 @@ public class IhomeConfigUtils {
 	 * 密码
 	 */
 	public final static String PASSWORD = "admin";
-	/**
-	 * http client 基础配置
-	 */
-	private static OkHttpClient client = new OkHttpClient.Builder().connectTimeout(30, TimeUnit.SECONDS)
-			.readTimeout(30, TimeUnit.SECONDS).retryOnConnectionFailure(true).writeTimeout(1500, TimeUnit.MILLISECONDS)
-			.build();
-	/**
-	 * basic 认证
-	 */
-	private static String credential = Credentials.basic(USERNAME, PASSWORD);
-	/**
-	 * request 认证头
-	 */
-	private static Request configRequest = new Request.Builder().url(CONFIG_URL).header("Authorization", credential)
-			.get().build();
-	private static Request envRequest = new Request.Builder().url(ENV_URL).header("Authorization", credential).get()
-			.build();
 	public final static String PROPERTY_NAME = "iray.server";
 	public final static String PROPERTY_LOADBALANCE_METHOD = "loadbalance.method";
 	public final static String PROPERTY_SPLIT_SYMBOL = ":";
 	public final static String IP_SPLIT_SYMBOL = ",";
 	public final static String CR = "\n";
 	public final static Integer HTTP_OK = 200;
+	/**
+	 * http client 基础配置
+	 */
+	static OkHttpClient client = new OkHttpClient.Builder().connectTimeout(60, TimeUnit.SECONDS)
+			.readTimeout(60, TimeUnit.SECONDS).retryOnConnectionFailure(true).writeTimeout(1500, TimeUnit.MILLISECONDS)
+			.build();
+	/**
+	 * basic 认证
+	 */
+	static String credential = Credentials.basic(USERNAME, PASSWORD);
+	/**
+	 * request 认证头
+	 */
+	static Request configRequest = new Request.Builder().url(CONFIG_URL).header("Authorization", credential).get()
+			.build();
 
-	static Timer timer = new Timer();
 	static String configStr;
-	static {
-		timer.scheduleAtFixedRate(new TimerTask() {
-			public void run() {
-				configStr = null;
-				commonParse(null);
-				log.info("configStr:{}", configStr);
-			}
+	public static Timer timer = new Timer();
+	{
+		try {
+			timer.scheduleAtFixedRate(new TimerTask() {
+				public void run() {
+					configStr = null;
+					commonParse(null);
+					log.warn("configStr:{}", configStr);
+				}
 
-		}, 5 * 1000, 1000 * 30);
+			}, 0, 1000 * 10);
+		} catch (Exception e) {
+			log.error("loop time error:{}", ExceptionUtils.getStackTrace(e));
+		}
 	}
 
 	public static void parseServeList(List<String> list) {
@@ -109,13 +110,14 @@ public class IhomeConfigUtils {
 					}
 				}
 			}
-			List<String> propertyValueList = Splitter.on(CR).splitToList(configStr);
-			propertyValueList.forEach(propertyValueStr -> {
-				if (parseConfigCallBack != null) {
-					parseConfigCallBack.doParse(propertyValueStr);
-				}
-			});
-
+			if (StringUtils.isNotBlank(configStr)) {
+				List<String> propertyValueList = Splitter.on(CR).splitToList(configStr);
+				propertyValueList.forEach(propertyValueStr -> {
+					if (parseConfigCallBack != null && StringUtils.isNotBlank(propertyValueStr)) {
+						parseConfigCallBack.doParse(propertyValueStr);
+					}
+				});
+			}
 		} catch (Exception e) {
 			log.error("query server config error:{}", ExceptionUtils.getStackTrace(e));
 		} finally {
@@ -123,7 +125,6 @@ public class IhomeConfigUtils {
 	}
 
 	private static ParseConfigCallBack extractedIrayServer(List<String> list) {
-		log.info("parse serverList:{}", Joiner.on(",").join(list));
 		return (propertyValueStr) -> {
 			if (propertyValueStr.contains(PROPERTY_NAME)) {
 				List<String> propertyValuePairList = Splitter.on(PROPERTY_SPLIT_SYMBOL).splitToList(propertyValueStr);
